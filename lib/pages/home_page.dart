a4bdfe029928cb0278f61e4349ac977eb30e35e0
Bryan Rodriguez <bsteven1993@gmail.com>
2020-08-22 10:00:47 -0500

    add list popular foods

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ui_food_delivery/core/constans.dart';
 import 'package:flutter_ui_food_delivery/core/flutter_icons.dart';
+import 'package:flutter_ui_food_delivery/models/food_model.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,6 +9,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final foodList = FoodModel.list;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,11 +20,112 @@ class _HomePageState extends State<HomePage> {
 
             _customAppBar(),
 
-            
+            Expanded(
+              child: ListView(
+                physics: BouncingScrollPhysics(),
+                children: [
+
+                  Text('Build pageview'),
+                  Padding(
+                    padding: EdgeInsets.only(left: 40),
+                    child: Text(
+                      'Popular',
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ),
+                  
+                  SizedBox(height: 16,),
+
+                  _buildPopularList()
+
+                ],
+              ),
+            )
 
           ],
         ),
       ),
+    );
+  }
+
+  ListView _buildPopularList() {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: BouncingScrollPhysics(),
+      padding: EdgeInsets.only(left: 40.0),
+      itemCount: foodList.length,
+      // scrollDirection: Axis.horizontal,
+      itemBuilder: ( _, index ){
+        return Container(
+          padding: EdgeInsets.all(16),
+          height: 120.0,
+          margin: EdgeInsets.only(bottom: 16),
+          decoration: BoxDecoration(
+            color: Colors.grey.withOpacity(.2),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(15),
+              bottomLeft: Radius.circular(15)
+            )
+          ),
+          child: Row(
+            children: [
+
+              Image(
+                width: 100,
+                image: AssetImage(
+                  'assets/images/${foodList[index].imgPath}'
+                ),
+              ),
+
+              SizedBox( width: 15,),
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+
+                  Text(
+                    foodList[index].name,
+                    style: TextStyle(
+                      fontSize: 20
+                    ),
+                  ),
+
+                  SizedBox( height: 5,),
+
+                  Row(
+                    children: [
+
+                      Text(
+                        '\$${foodList[index].price.toInt()}',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: AppColors.redColor,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+
+                      SizedBox( width: 15,),
+
+                      Text(
+                        '${foodList[index].weight.toInt()} gm Weight',
+                        style: TextStyle(
+                          fontSize: 12
+                        ),
+                      ),
+
+                    ],
+                  )
+
+                ],
+              )
+
+            ],
+          ),
+        );
+      },
     );
   }
 
@@ -72,7 +176,10 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                   
-                  Icon( Icons.search)
+                  Icon( 
+                    FlutterIcons.search,
+                    size: 16,
+                  )
 
 
                 ],
